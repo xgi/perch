daee5467b1bc9d79a5aa39e9f42cea5da62ebdfd
Jake Robertson <jake@faltro.com>
2019-05-22 17:32:18 -0400

    remove image/cloudanchor activities

--- app/src/main/java/com/faltro/perch/activity/menu/MainActivity.kt
+++ app/src/main/java/com/faltro/perch/activity/menu/MainActivity.kt
@@ -5,26 +5,16 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import com.faltro.perch.R
-import com.faltro.perch.activity.CloudAnchorActivity
-import com.faltro.perch.activity.ImageActivity
 import com.faltro.perch.activity.SceneformActivity
 
 class MainActivity : AppCompatActivity() {
-    private val items: Array<String> = arrayOf("Sceneform", "Augmented Images", "Cloud Anchors")
+    private val items: Array<String> = arrayOf("Sceneform")
 
     private val adapter by lazy {
         MenuAdapter(items) {
             when (it) {
                 0 -> {
                     val intent = Intent(this, SceneformActivity::class.java)
-                    startActivity(intent)
-                }
-                1 -> {
-                    val intent = Intent(this, ImageActivity::class.java)
-                    startActivity(intent)
-                }
-                2 -> {
-                    val intent = Intent(this, CloudAnchorActivity::class.java)
                     startActivity(intent)
                 }
             }
--- app/src/main/java/com/faltro/perch/activity/menu/MenuAdapter.kt
+++ app/src/main/java/com/faltro/perch/activity/menu/MenuAdapter.kt
@@ -5,7 +5,7 @@ import android.view.ViewGroup
 
 class MenuAdapter(private val items: Array<String>, private val onItemClick: (Int) -> Unit) : RecyclerView.Adapter<MenuViewHolder>() {
 
-    override fun getItemCount() = 3
+    override fun getItemCount() = items.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder =
             MenuViewHolder.create(parent, items, onItemClick)
